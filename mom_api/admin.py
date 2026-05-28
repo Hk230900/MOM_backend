4c5497e659f5d3b7d465bbdc7cd779e4d71aa40e
Harshada Bhalchandra Kale <162621291+Hk230900@users.noreply.github.com>
2026-05-29 01:26:27 +0530

    Register models in Django admin

--- mom_api/admin.py
+++ mom_api/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from .models import ProjectDetail, MeetingDetail, UserProfile
+
+admin.site.register(ProjectDetail)
+admin.site.register(MeetingDetail)
+admin.site.register(UserProfile)
